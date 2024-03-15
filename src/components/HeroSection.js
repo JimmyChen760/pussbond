3167615d1c239fde53fc4e2a8b998097f2638ddb
RockChewer <jimmychen760@gmail.com>
2024-03-14 20:46:05 -0400

    Fourth Commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,6 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
+      <video src='/videos/video-4.mp4' autoPlay loop muted />
       <h1 className='hero-header'>PUSSBOND IS WAITING</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
